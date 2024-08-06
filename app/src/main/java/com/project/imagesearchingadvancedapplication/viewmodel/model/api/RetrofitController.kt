acebb6b4998011f042961a8f0e6113239ac2402c
Rure00 <asnjkd5193@gmail.com>
2024-08-06 10:52:40 +0900

    변경: imageData 시간 포맷 변경

--- app/src/main/java/com/project/imagesearchingadvancedapplication/viewmodel/model/api/RetrofitController.kt
+++ app/src/main/java/com/project/imagesearchingadvancedapplication/viewmodel/model/api/RetrofitController.kt
@@ -7,21 +7,28 @@ class RetrofitController {
     private val client = RetrofitClient.getInstance().create(RetrofitService::class.java)
 
     suspend fun getImages(query: String): List<ImageData>
-        = client.getImages(query).documents.map {
+    = client.getImages(query).documents.map {
+        val time = it.datetime.split(".")[0].split("T")
+            .joinToString(" ")
+
             ImageData(
                 imageUrl = it.image_url,
                 from = it.display_sitename,
-                time = it.datetime,
+                time = time,
                 category = ImageData.Category.Image
             )
         }
 
     suspend fun getVideos(query: String): List<ImageData>
-            = client.getVideos(query).documents.map {
+    = client.getVideos(query).documents.map {
+        val time = it.datetime.split(".")[0].split("T")
+            .joinToString(" ")
+
+
         ImageData(
             imageUrl = it.thumbnail,
             from = "[${it.author}] ${it.title}",
-            time = it.datetime,
+            time = time,
             category = ImageData.Category.Video
         )
     }
