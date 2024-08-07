bc1fdd894ba04015a56e7b45ef508143b6683552
Rure00 <asnjkd5193@gmail.com>
2024-08-07 19:25:17 +0900

    완료: AppRepository 정의

--- app/src/main/java/com/project/imagesearchingadvancedapplication/data/repository/AppRepository.kt
+++ app/src/main/java/com/project/imagesearchingadvancedapplication/data/repository/AppRepository.kt
@@ -1,5 +1,11 @@
 package com.project.imagesearchingadvancedapplication.data.repository
 
-interface AppRepository {
+import android.provider.ContactsContract.CommonDataKinds.Im
+import com.project.imagesearchingadvancedapplication.domain.model.ImageData
 
+interface AppRepository {
+    suspend fun getImageData(query: String, size: Int): List<ImageData>
+    fun saveImageData(list: List<ImageData>)
+    fun getLastQuery(): String
+    fun saveLastQuery(query: String)
 }
--- app/src/main/java/com/project/imagesearchingadvancedapplication/data/repository/AppRepositoryImpl.kt
+++ app/src/main/java/com/project/imagesearchingadvancedapplication/data/repository/AppRepositoryImpl.kt
@@ -2,11 +2,29 @@ package com.project.imagesearchingadvancedapplication.data.repository
 
 import com.project.imagesearchingadvancedapplication.data.source.LocalDataSource
 import com.project.imagesearchingadvancedapplication.data.source.RemoteDataSource
+import com.project.imagesearchingadvancedapplication.domain.model.ImageData
 import javax.inject.Inject
 
 class AppRepositoryImpl @Inject constructor(
     private val remoteDataSource: RemoteDataSource,
     private val localDataSource: LocalDataSource
 ): AppRepository {
+    override suspend fun getImageData(query: String, size: Int): List<ImageData> {
+        val images = remoteDataSource.getImages(query, size)
+        val videos = remoteDataSource.getVideos(query, size)
 
+        return (images + videos).sortedByDescending { it.time }
+    }
+
+    override fun saveImageData(list: List<ImageData>) {
+        localDataSource.saveImages(list)
+    }
+
+    override fun getLastQuery(): String
+        = localDataSource.getLastQuery() ?: ""
+
+
+    override fun saveLastQuery(query: String) {
+        localDataSource.saveQuery(query)
+    }
 }
